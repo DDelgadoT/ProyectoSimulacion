049e773a5373c6b4120c876055c353f325142346
DDelgadoT <diego.fernando.delgado@correounivalle.edu.co>
2021-12-19 22:00:56 -0500

    Arreglos para que solo funcionen X maquinas al tiempo

--- maquinas.py
+++ maquinas.py
@@ -1,9 +1,6 @@
 import random
 import simpy
 
-# REVISAR SI LA MAQUINA ES UN NUMERO != 0 PARA SABER QUE HAY UNA MAQUINA AHI, EN EL CASO QUE SEA 0, SE SABE QUE NO HAY MAQUINA
-# CREAR UN ARREGLO CON NÚMEROS DE LOS LUGARES VACIOS
-
 tab = "             "
 
 maquinasEnFabrica = []
@@ -14,7 +11,7 @@ cantidadMaq = 5
 maquinasFuncionando = 0
 cantidadRep = 2
 reparadores = 3
-tiempo = 180
+tiempo = 360
 
 for i in range(cantidadMaq):
     maquinasEnFabrica.append(i+1)
@@ -23,41 +20,54 @@ for i in range(cantidadRep):
     maquinasDeRepuesto.append("Repuesto%02d" % i)
 
 def maquinasEnFuncionamiento(env, counter):
+
+    global maquinasFuncionando
     
     while len(maquinasEnFabrica) != 0:
-        duracionArreglo = random.randrange(5, 11)
+        duracionArreglo = random.uniform(5, 11)
         c = repair(env, maquinasEnFabrica[0], counter, duracionArreglo)
         env.process(c)
-        global maquinasFuncionando
+        
         maquinasFuncionando += 1
         yield env.timeout(0)
         maquinasEnFabrica.pop(0)
 
 
 def repair(env, name, counter, time_in_repair):
-    arrive = env.now
 
-    duracionMaquina = random.randrange(130, 190)    # Genera un número al azar para simular el tiempo de funcionamiento de la máquina
+    arrive = env.now
+    global maquinasFuncionando
+
+    if(type(name) == int):
+        duracionMaquina = random.uniform(130, 190)    # Genera un número al azar para simular el tiempo de funcionamiento de la máquina
+    else:
+        duracionMaquina = random.uniform(190, 250)
+
     print('%10.0f %s %s: Funcionando ' % (arrive, tab, name))
     yield env.timeout(duracionMaquina)
+
+    print()
+    print("Máquinas funcionando en la fábrica: %s" % maquinasFuncionando)
+    print()
 
     arrive = env.now
     print('%10.0f %s %s: Se dañó máquina ' % (arrive, tab, name))
     espaciosVacios.append(name)     # Agrega la máquina que se acaba de dañar
-    global maquinasFuncionando
+    
     maquinasFuncionando -= 1
 
     print()
-    print("Número de máquinas de repuesto disponibles: %s" % len(maquinasDeRepuesto))
+    print("Máquinas de repuesto disponibles: %s" % maquinasDeRepuesto)
     print()
 
     if len(maquinasDeRepuesto) != 0 and maquinasFuncionando != cantidadMaq:
         maquinasEnFabrica.append(maquinasDeRepuesto.pop(0))
-        duracionArreglo = random.randrange(5, 11)
+        duracionArreglo = random.uniform(5, 11)
         c = repair(env, maquinasEnFabrica[0], counter, duracionArreglo)
         env.process(c)
         yield env.timeout(0)
         maquinasEnFabrica.pop(0)
+        maquinasFuncionando += 1
 
     with counter.request() as req:
         results = yield req
@@ -81,9 +91,10 @@ def repair(env, name, counter, time_in_repair):
 
         if len(maquinasDeRepuesto) == 0 and maquinasFuncionando != cantidadMaq:
             maquinasEnFabrica.append(name)
-            duracionArreglo = random.randrange(5, 11)
+            duracionArreglo = random.uniform(5, 11)
             c = repair(env, maquinasEnFabrica[0], counter, duracionArreglo)
             env.process(c)
+            maquinasFuncionando += 1
         else:
             maquinasDeRepuesto.append(name)
 
